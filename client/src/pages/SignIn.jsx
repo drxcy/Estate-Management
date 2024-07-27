2a2ced330594958ab7084ccd14fc91946f71a8d2
drxcy <rachitbahuguna5@gmail.com>
2024-07-27 12:05:35 +0530

    Create Sign In Page UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useState } from "react";
+import { Link,useNavigate} from "react-router-dom";
 
-export default function SignIn() {
+export default function SignUp() {
+  const [formdata, setformdata] = useState({});
+  const navigate =useNavigate();
+  const submitHandler = async (e) => {
+    e.preventdefault();
+    try {
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      if(data.success==false){
+        alert(data.message);
+      }
+      if(data.success){
+        navigate('/');
+      }
+      
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const changeHandler = (e) => {
+    setformdata({ ...formdata, [e.target.id]: e.target.value });
+  };
+
   return (
-    <div>
-      Sign In
+    <div className="w-max-lg mx-auto p-3">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onChange={submitHandler}>
+        
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={changeHandler}
+          className="rounded-lg border p-3"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={changeHandler}
+          className="rounded-lg border p-3"
+        />
+        <button
+          type="Submit"
+          className="bg-blue-900 text-white p-3 font-semibold rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className="flex space-x-1 mt-5 font-semibold">
+        <p className="text-left">Dont have an account?</p>{" "}
+        <Link to="Sign-up">
+          <span className="text-blue-900 hover:text-blue-700">Sign Up</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
+
